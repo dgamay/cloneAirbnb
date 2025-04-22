f3de51250e303a0f59cd708cc6e36305d4ca4b46
dgamay <dgamay10@gmail.com>
2025-04-21 21:39:15 -0500

    Esquema habitaciones creado

--- back_end/models/airbnb.model.js
+++ back_end/models/airbnb.model.js
@@ -2,6 +2,47 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const CloneairbnbSchema = new Schema ({
-    
+const habitacionSchema = new Schema ({
+    nombre:{
+        type:String,
+        require: true,
+        unique: true,
+    },
+
+    capacidad: {
+        type:Number,
+        enum:[1, 2, 4],
+        require: true,
+    },
+
+    baño:{
+        type:String,
+        enum: ['privado','compartido'],
+        require: true,
+    },
+    servicios:{
+        type    :String,
+        enum    :['desayuno', 'internet','tv','lavanderia','ninguno'],
+        require :true,
+    },
+
+    precio :{
+        type :Number,
+        require : true,
+    },
+    fotos :{
+        type : [{ type: String }], // Arreglo dinámico para agregar las imagenes de las habitaciones
+        require : true
+    }
+
+
+
 })
+
+/* - nombre de la habitación
+- capacidad (cantidad de personas por habitación)
+- baño (privado o compartido)
+- servicios incluidos (desayuno, internet, tv, lavandería)
+- precio
+- foto principal
+- fotos auxiliares */
